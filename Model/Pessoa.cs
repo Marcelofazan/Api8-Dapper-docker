a363f3aa77ca846dc61dc59025833661067fb4a4
Marcelo Fazan <56393266+Marcelofazan@users.noreply.github.com>
2026-06-27 15:45:09 -0300

    Initialize string properties in Pessoa class
    Updated properties in Pessoa class to initialize with empty strings.

--- Model/Pessoa.cs
+++ Model/Pessoa.cs
@@ -11,22 +11,22 @@ namespace ApiMySql.Model
 
         [Required]
         [StringLength(80)]
-        public string RazaoSocial { get; set; }
+        public string RazaoSocial { get; set; } = string.Empty;
 
         [StringLength(18)]
-        public string CnpjCpf { get; set; }
+        public string CnpjCpf { get; set; } = string.Empty;
 
         [StringLength(120)]
-        public string Email { get; set; }
+        public string Email { get; set; } = string.Empty;
 
         [StringLength(15)]
-        public string Telefone { get; set; }
+        public string Telefone { get; set; } = string.Empty;
 
         [StringLength(100)]
-        public string Usuario { get; set; }
+        public string Usuario { get; set; } = string.Empty;
 
         [StringLength(12)]
-        public string Senha { get; set; }
+        public string Senha { get; set; } = string.Empty;
 
     }
 }
