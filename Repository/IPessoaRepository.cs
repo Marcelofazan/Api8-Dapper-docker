d617bc735e2f1604209359584a3e3ea640f0a79c
Marcelo Fazan <56393266+Marcelofazan@users.noreply.github.com>
2026-06-27 15:43:31 -0300

    Make BuscaPorId return nullable Pessoa

--- Repository/IPessoaRepository.cs
+++ Repository/IPessoaRepository.cs
@@ -6,7 +6,7 @@ namespace ApiMySql.Repository
     public interface IPessoaRepository
     {
         IEnumerable<Pessoa> Busca();
-        Pessoa BuscaPorId(int idpessoa);
+        Pessoa? BuscaPorId(int idpessoa);
         void Inserir(Pessoa Pessoas);
         void Excluir(int idpessoa);
         void Alterar(int idpessoa, Pessoa Pessoas);
